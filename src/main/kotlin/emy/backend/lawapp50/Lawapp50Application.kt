1c495f3d9d83d971d1a781a1fb84fc42f343a6c2
ElieOko <elieoko100@gmail.com>
2026-02-28 14:59:36 +0100

    build:creation du projet

---
+++ src/main/kotlin/emy/backend/lawapp50/Lawapp50Application.kt
@@ -0,0 +1,11 @@
+package emy.backend.lawapp50
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Lawapp50Application
+
+fun main(args: Array<String>) {
+	runApplication<Lawapp50Application>(*args)
+}
---
+++ src/test/kotlin/emy/backend/lawapp50/Lawapp50ApplicationTests.kt
@@ -0,0 +1,13 @@
+package emy.backend.lawapp50
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Lawapp50ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
