8a28adbdd54bf1e7245b08dced4145985d73d77d
ElieOko <elieoko100@gmail.com>
2026-03-07 18:20:46 +0100

    feat:contruction de l'architecture du module school ecosytem

---
+++ src/main/kotlin/emy/backend/lawapp50/app/school_ecosystem/infrastructure/persistance/entity/PromotionEntity.kt
@@ -0,0 +1,14 @@
+package emy.backend.lawapp50.app.school_ecosystem.infrastructure.persistance.entity
+
+import org.springframework.data.annotation.Id
+import org.springframework.data.relational.core.mapping.Column
+
+class PromotionEntity (
+    @Id
+    @Column("id")
+    val id: Long? = null,
+    @Column("name")
+    val name: String,
+    @Column("is_active")
+    val isActive: Boolean = true
+)
