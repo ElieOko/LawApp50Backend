e95b9704aa6ccaf2c1c456a29a14bb1b6f2df09d
mr zou <a.tshiyanze@gmail.com>
2026-03-08 16:21:02 +0100

    response commentaire contenu

--- src/main/kotlin/emy/backend/lawapp50/app/contenus/domain/model/CommentaireResponseContenu.kt
+++ src/main/kotlin/emy/backend/lawapp50/app/contenus/domain/model/CommentaireResponseContenu.kt
@@ -1,1 +1,20 @@
 package emy.backend.lawapp50.app.contenus.domain.model
+
+import jakarta.validation.constraints.NotNull
+
+data class CommentaireResponseContenu(
+    val id: Long? = null,
+    var commentaireContenuId : Long,
+    var userId: Long,
+    var description : String,
+    var isActive: Boolean = true,
+)
+
+data class CommentaireResponseContenuRequest(
+    @NotNull
+    var commentaireContenuId : Long,
+    @NotNull
+    var userId: Long,
+    @NotNull
+    var description : String,
+)
