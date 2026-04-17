17571a6fd86b70689ee0fe419becd6cdb0ef2b41
ElieOko <elieoko100@gmail.com>
2026-04-17 13:34:49 +0100

    fix:comment config DB

--- src/main/kotlin/emy/backend/lawapp50/config/ConfigDB.kt
+++ src/main/kotlin/emy/backend/lawapp50/config/ConfigDB.kt
@@ -1,9 +1,11 @@
 package emy.backend.lawapp50.config
 
+import org.flywaydb.core.Flyway
 import org.springframework.context.annotation.Configuration
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.flyway.autoconfigure.FlywayProperties
 import org.springframework.boot.r2dbc.autoconfigure.R2dbcProperties
+import org.springframework.context.annotation.Bean
 
 @Configuration
 @EnableConfigurationProperties(
@@ -12,20 +14,20 @@ import org.springframework.boot.r2dbc.autoconfigure.R2dbcProperties
 )
 class DatabaseConfig {
 
-//    @Bean(initMethod = "migrate")
-//    fun flyway(
-//        flywayProperties: FlywayProperties,
-//        r2dbcProperties: R2dbcProperties
-//    ): Flyway {
-//
-//        return Flyway.configure()
-//            .dataSource(
-//                "jdbc:postgresql://aws-1-eu-central-1.pooler.supabase.com:5432/postgres",          // JDBC URL
-//                r2dbcProperties.username,
-//                r2dbcProperties.password
-//            )
-//            .locations(*flywayProperties.locations.toTypedArray())
-//            .baselineOnMigrate(true)
-//            .load()
-//    }
+  /*  @Bean(initMethod = "migrate")
+    fun flyway(
+        flywayProperties: FlywayProperties,
+        r2dbcProperties: R2dbcProperties
+    ): Flyway {
+
+        return Flyway.configure()
+            .dataSource(
+                "jdbc:postgresql://aws-1-eu-central-1.pooler.supabase.com:5432/postgres",          // JDBC URL
+                r2dbcProperties.username,
+                r2dbcProperties.password
+            )
+            .locations(*flywayProperties.locations.toTypedArray())
+            .baselineOnMigrate(true)
+            .load()
+    }*/
 }
